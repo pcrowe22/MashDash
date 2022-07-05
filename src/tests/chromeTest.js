0cfefec9b93cfadaba09ce9238d348ca304f5f64
Patrick Crowe <pcrowe@hawk.iit.edu>
2022-07-05 16:51:24 -0500

    Full require

--- src/tests/chromeTest.js
+++ src/tests/chromeTest.js
@@ -1,8 +1,8 @@
-const webdriver = require('selenium-webdriver');/*,
+const webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
 
-const driver = new webdriver.Builder()
+/*const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 driver.get('http://www.google.com').then(function(){
