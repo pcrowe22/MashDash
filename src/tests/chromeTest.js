dccdf67d999a0839615a257edb9b8827b46978c3
Patrick Crowe <pcrowe@hawk.iit.edu>
2022-07-05 16:52:39 -0500

    Built driver

--- src/tests/chromeTest.js
+++ src/tests/chromeTest.js
@@ -2,10 +2,10 @@ const webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
 
-/*const driver = new webdriver.Builder()
+const driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
-driver.get('http://www.google.com').then(function(){
+/*driver.get('http://www.google.com').then(function(){
 driver.findElement(webdriver.By.name('q')).sendKeys('webdriver\n').then(function(){
     driver.getTitle().then(function(title) {
       console.log(title)
