964a7842f0a82f9626d7875b83b9e827617011ea
Patrick Crowe <pcrowe@hawk.iit.edu>
2022-07-05 17:10:29 -0500

    Added browser options

--- src/tests/chromeTest.js
+++ src/tests/chromeTest.js
@@ -2,8 +2,13 @@ const webdriver = require('selenium-webdriver'),
     By = webdriver.By,
     until = webdriver.until;
 
+const chrome = require('selenium-webdriver/chrome');
+let opts = new chrome.Options();
+opts.headless = true;
+
 const driver = new webdriver.Builder()
     .forBrowser('chrome')
+    .setChromeOptions(opts)
     .build();
 /*driver.get('http://www.google.com').then(function(){
 driver.findElement(webdriver.By.name('q')).sendKeys('webdriver\n').then(function(){
