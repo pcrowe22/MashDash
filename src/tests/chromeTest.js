3415558d964c72c047f3a1c46db8353667bf19f3
Patrick Crowe <pcrowe@hawk.iit.edu>
2022-07-05 21:28:08 -0500

    removed window size

--- src/tests/chromeTest.js
+++ src/tests/chromeTest.js
@@ -8,7 +8,7 @@ const webdriver = require('selenium-webdriver'),
 const chrome = require('selenium-webdriver/chrome');
 let opts = new chrome.Options();
 opts.headless();
-opts.addArguments('window-size=1920x1080');
+//opts.addArguments('window-size=1920x1080');
 
 const driver = new webdriver.Builder()
     .forBrowser('chrome')
