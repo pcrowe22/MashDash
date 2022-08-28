5758d80b24b452709e54175a56bdc2de0c747895
Patrick Crowe <pcrowe@hawk.iit.edu>
2022-08-28 16:53:10 -0500

    Converted element in test to element array

--- src/tests/chromeTest.js
+++ src/tests/chromeTest.js
@@ -88,7 +88,7 @@ driver.get('http://127.0.0.1:8888').then(function(){
         
         // genre test
         .then(() => driver.findElements(By.name('genre-checkbox')))
-        .then((element) => driver.executeScript("arguments[0].scrollIntoView(true);", element))
+        .then((elements) => driver.executeScript("arguments[0].scrollIntoView(true);", elements[0]))
         .then(() => driver.sleep(1000))
 
         .then(() => driver.findElements(By.name('genre-checkbox')))
