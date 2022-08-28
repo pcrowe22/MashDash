51b9d80e98b53c013c09cbb4be85b4b4435aaac6
Patrick Crowe <pcrowe@hawk.iit.edu>
2022-08-28 16:48:45 -0500

    Adjusted genre test

--- src/tests/chromeTest.js
+++ src/tests/chromeTest.js
@@ -88,6 +88,10 @@ driver.get('http://127.0.0.1:8888').then(function(){
         
         // genre test
         .then(() => driver.findElements(By.name('genre-checkbox')))
+        .then((element) => driver.executeScript("arguments[0].scrollIntoView(true);", element))
+        .then(() => driver.sleep(1000))
+
+        .then(() => driver.findElements(By.name('genre-checkbox')))
         .then((inputs) => {
             console.log("Genre test");
             return inputs[0].click();
